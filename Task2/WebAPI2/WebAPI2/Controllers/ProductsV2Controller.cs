2d120ae1221bb6196607b5ec7854373edcd87f3d
WeiHanWong <56218253+WeiHanWong@users.noreply.github.com>
2019-12-03 22:09:44 +0800

    Task2 50%

---
+++ Task2/WebAPI2/WebAPI2/Controllers/ProductsV2Controller.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebAPI2.Models;
+
+namespace WebAPI2.Controllers
+{
+    public class ProductsV2Controller : ApiController
+    {
+        static readonly IProductRepository repository = new ProductRepository();
+
+        [HttpGet]
+        [Route("api/V2/products")]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return repository.GetAll();
+        }
+
+        [HttpGet]
+        [Route("api/v2/products/{id:int:min(1)}", Name = "getProductById")]
+        public Product GetProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return item;
+        }
+
+        [HttpGet]
+        [Route("api/v2/products", Name = "getProductByCategory")]
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return repository.GetAll().Where(
+                p => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
+        }
+
+        [HttpPost]
+        [Route("api/v2/products")]
+        public HttpResponseMessage PostProduct(Product item)
+        {
+            item = repository.Add(item);
+            var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item);
+
+            string uri = Url.Link("getProductById", new { id = item.Id });
+            response.Headers.Location = new Uri(uri);
+            return response;
+        }
+
+        [HttpPut]
+        [Route("api/v2/products/{id:int}")]
+        public void PutProduct(int id, Product product)
+        {
+            product.Id = id;
+            if (!repository.Update(product))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            ////Demo Purpose
+            //var response = Request.CreateResponse<Product>(HttpStatusCode.OK, product);
+
+            //string uri = Url.Link("getProductById", new { id = id });
+            //response.Headers.Location = new Uri(uri);
+            //return response;
+        }
+
+        [HttpDelete]
+        [Route("api/v2/products/{id:int}")]
+        public void DeleteProduct(int id)
+        {
+            repository.Remove(id);
+            
+            ////Demo Purpose
+            //return repository.GetAll();
+        }
+    }
+}
