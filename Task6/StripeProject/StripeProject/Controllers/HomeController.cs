48395cd6765cd65d897be066360cea828028d3bd
WeiHanWong <56218253+WeiHanWong@users.noreply.github.com>
2019-12-12 13:56:59 +0800

    75% done task 6

---
+++ Task6/StripeProject/StripeProject/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System.Web.Mvc;
+
+namespace StripeProject.Controllers
+{
+    using System.Threading.Tasks;
+    using Models;
+    using Stripe;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Charge()
+        {
+            ViewBag.Message = "Learn how to process payments with Stripe";
+
+            return View(new StripeChargeModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Charge(StripeChargeModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var chargeId = await ProcessPayment(model);
+            return View("Index");
+        }
+
+        private async Task<string> ProcessPayment(StripeChargeModel model)
+        {
+            return await Task.Run(() =>
+            {
+                var myCharge = new StripeChargeCreateOptions
+                {
+                    // convert the amount of £12.50 to pennies i.e. 1250
+                    Amount = (int)(model.Amount * 100),
+                    Currency = "sgd",
+                    Description = "Description for test charge",
+                    Source = new StripeSourceOptions
+                    {
+                        TokenId = model.Token
+                    }
+                };
+
+                var chargeService = new StripeChargeService("sk_test_dVjm658ONAh8pNkRvH2iHfWR00S7dGPJI0");
+                var stripeCharge = chargeService.Create(myCharge);
+
+                return stripeCharge.Id;
+            });
+        }
+    }
+}
