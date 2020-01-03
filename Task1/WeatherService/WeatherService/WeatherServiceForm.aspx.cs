9cf3b8d45bbfe0dd74088e4aae72f9db0c1f1ef6
WeiHanWong <56218253+WeiHanWong@users.noreply.github.com>
2020-01-03 13:20:31 +0800

    add

--- Task1/WeatherService/WeatherService/WeatherServiceForm.aspx.cs
+++ Task1/WeatherService/WeatherService/WeatherServiceForm.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Threading;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -16,13 +17,18 @@ namespace WeatherService
             XmlDocument wsResponseXmlDoc = new XmlDocument();
 
             //http://api.worldweatheronline.com/premium/v1/weather.ashx?key=****&q=London&format=xml&num_of_days=5
-            //id=jipx(spacetime0)
+
+            //Assign values for query
+            var country = "china";
+            var numOfDays = "5";
+            var key = "cc0e4957de9e41d4a3d71549190912";
+
             UriBuilder url = new UriBuilder();
-            url.Scheme = "http";// Same as "http://"
+            url.Scheme = "http";
 
             url.Host = "api.worldweatheronline.com";
-            url.Path = "premium/v1/weather.ashx";// change to v2
-            url.Query = "q=china&format=xml&num_of_days=5&key=cc0e4957de9e41d4a3d71549190912";
+            url.Path = "premium/v1/weather.ashx";
+            url.Query = "q="+ country + "&format=xml&num_of_days="+ numOfDays + "&key=" + key;
 
             //Make a HTTP request to the global weather web service
             wsResponseXmlDoc = MakeRequest(url.ToString());
@@ -38,14 +44,17 @@ namespace WeatherService
                 writer.Formatting = Formatting.Indented;
                 wsResponseXmlDoc.Save(writer);
 
-                //You're never closing the writer, so I would expect it to keep the file open. That will stop future attempts to open the file
-
                 writer.Close();
             }
             else
             {
                 Response.ContentType = "text/html";
                 Response.Write("<h2> error  accessing web service </h2>");
+                Response.Write("<p>Please check if your API key is incorrect: " + key + "</p>");
+                Response.Write("<p>Please check if your country spelled incorrectly: " + country + "</p>");
+                Response.Write("<p>Please check if your number of days is in integer form: " + numOfDays + "</p>");
+                Response.Write("<b>Rectify and re-run the project</b>");
+                Response.End();
             }
 
         }
